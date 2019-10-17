32b164e9e6a455998d449ad2f03a3e2ba13c03b3
romankalab <roan.kalab@mail.com>
2019-10-17 16:12:23 +0200

    Initial

--- Open-Lab-02.7/Program.cs
+++ Open-Lab-02.7/Program.cs
@@ -12,16 +12,13 @@ namespace Open_Lab_02._7
             Console.WriteLine("Enter month:");
             int month = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter year:");
-            int year = Convert.ToInt32(Console.ReadLine());
-
-            bool answer = IsTimeForMilkAndCookies(year, month, day);
+            bool answer = IsTimeForMilkAndCookies(month, day);
 
             Console.WriteLine($"{answer}");
         }
-        private static bool IsTimeForMilkAndCookies(int year, int month, int day)
+        private static bool IsTimeForMilkAndCookies(int month, int day)
         {
-            return year == 2019 && month == 12 && day == 24;
+            return month == 12 && day == 24;
         }
     }
 }
